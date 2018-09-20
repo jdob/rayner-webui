998827f244103902b8e93199315916e0aeabfbc4
Jay Dobies <jason.dobies@redhat.com>
2018-09-20 15:05:55 -0400

    Look up current light color from the light API

--- src/Status.js
+++ src/Status.js
@@ -4,14 +4,28 @@ import React, { Component } from 'react';
 class Status extends Component {
     constructor(props) {
         super(props);
-        this.current_color = "ffffff";
+        this.state = {
+            current_color: "ffffff",
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8000/light/')
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            let c = data.hex
+            this.setState({current_color: c})
+            console.log('current_color', this.state.current_color)
+        })
     }
 
     render() {
         return(
             <div align="center">
                 <div>Current Light Color</div>
-                <div className="status-box"></div>
+                <div className="status-box"
+                     style={{background: '#' + this.state.current_color}}></div>
             </div>
         )
     }
