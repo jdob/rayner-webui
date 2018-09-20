cade0d6a541ac0d201ea2cb7fd1d98b870d7967c
Jay Dobies <jason.dobies@redhat.com>
2018-09-20 14:33:41 -0400

    Minor graphical tweaks

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
   render() {
     return (
         <div className="container">
+            <div className="page-header">
+                <h1>Rayner Dashboard</h1>
+            </div>
             <div className="row">
                 <div className="col-md-8"><EventsTable/></div>
                 <div className="col-md-4"><Status/></div>
